e48fe9b7b55a0cdd657d243d310d96932cd3c425
martksk <krtsk2547@gmail.com>
2019-11-10 14:42:46 +0700

    from 0 to 10

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 10;
   String _greeting='hi';
   bool _increUp=true;
 
